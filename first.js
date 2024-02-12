0cac3dc47b0e144e7320a25863ed9210c0f72607
Osfoce <oselufortunatus2002@gmail.com>
2024-02-12 11:42:45 +0100

    Task-1; Looping a triangle

---
+++ first.js
@@ -0,0 +1,16 @@
+let var1 = 7, out = "#";
+for (x = 0; x < var1; x++)
+{
+	console.log(out);
+	out += "#";
+}
+/*Looping a triangle
+Write a loop that makes seven calls to console.log to output the following
+triangle:
+#
+##
+###
+####
+#####
+######
+#######*/
